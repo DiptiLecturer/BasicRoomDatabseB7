78589720b28312c49004d7f021176a7416b40578
DiptiLecturer <lecturer3.amad.nsda@dipti.com.bd>
2026-02-25 11:09:06 +0600

    "done"

---
+++ app/src/main/java/org/freedu/basicroomdbb7/NoteRepository.kt
@@ -0,0 +1,19 @@
+package org.freedu.basicroomdbb7
+
+class NoteRepository(private val noteDao: NoteDao) {
+
+
+    fun getAllNotesFromRepo(): List<Note>{
+        return noteDao.getAllNotes()
+    }
+    fun insert(note: Note){
+        noteDao.insert(note)
+    }
+    fun update(note: Note){
+        noteDao.update(note)
+    }
+    fun delete(note: Note){
+        noteDao.delete(note)
+    }
+
+}
